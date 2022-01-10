000e7e6d6a19b7a194904f805bba7da9aea30e32
ivneld <ivneld@naver.com>
2022-01-10 21:49:47 +0900

    p_3

---
+++ Java_100/src/Java100_array02.java
@@ -0,0 +1,6 @@
+public class Java100_array02 {
+	static int[] sales_table= {33,52,93,100,87};
+	static int s_size=sales_table.length;
+	
+	
+}
---
+++ Java_100/src/Java100_array03.java
@@ -0,0 +1,6 @@
+public class Java100_array03 {
+	
+	 static int[] arr= {3,3,3,3,4};
+	static double[] arr2= {9.8,2.45,5.98,5.11,3.14};
+	
+}
